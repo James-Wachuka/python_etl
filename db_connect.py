89192c9eeb2192d477c98c180be9526142434cfa
James Wachuka <jewachu26@gmail.com>
2022-09-24 16:41:23 +0300

    initial commit

--- db_connect.py
+++ db_connect.py
@@ -1,16 +1,16 @@
 
-#import pymysql
 import psycopg2
 import mysql.connector
 
+# source database
 def get_conn_mysql():
-    conn = mysql.connector.connect(host="localhost", port=3306, user="root", password="jewachu26", db="classicmodels")
+    conn = mysql.connector.connect(host="localhost", port=3306, user="root", password="", db="classicmodels")
     # start a connection
     cur = conn.cursor()
     return cur, conn
-
+# target database
 def get_conn_postgresql():
-    conn = psycopg2.connect(host="localhost",database="classicmodels_analysis",user="postgres",password="jewachu26")
+    conn = psycopg2.connect(host="localhost",database="classicmodels_analysis",user="postgres",password="")
     # start a connection
     cur = conn.cursor()
     return cur, conn
